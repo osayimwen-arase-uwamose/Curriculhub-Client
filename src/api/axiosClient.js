1c482db8fb4c5836e72e0d7f264c498e40720eca
dayone.codes@gmail.com <dayone.codes@gmail.com>
2026-08-18 15:54:39 +0100

    Feat(Axios): Add Axios client, Initialize with api endpoints

---
+++ src/api/axiosClient.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+
+  withCredentials: true,
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+
+  timeout: 15000,
+});
+
+export default axiosClient;
---
+++ src/data/endpoints.js
@@ -0,0 +1,19 @@
+const ENDPOINTS = {
+  AUTH: {
+    LOGIN: "/auth/login",
+    REGISTER:"/auth/register",
+    LOGOUT: "/auth/logout",
+    REFRESH: "/auth/refresh",
+    PROFILE: "/auth/profile",
+  },
+
+  STUDENTS: {
+    BASE: "/students",
+  },
+
+  COURSES: {
+    BASE: "/courses",
+  },
+};
+
+export default ENDPOINTS;
